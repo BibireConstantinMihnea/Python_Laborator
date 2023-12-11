c36983e309971d280e48e678e532ebf41519152a
BibireConstantinMihnea <100199858+BibireConstantinMihnea@users.noreply.github.com>
2023-12-11 18:52:46 +0200

    added reddit api integration

---
+++ Proiect/api_requests.py
@@ -0,0 +1,35 @@
+import requests
+
+def reddit_news_fetch(subject):
+    base_url = 'https://www.reddit.com/'
+    with open('pw.txt', 'r') as f:
+        pw = f.read()
+    data = {'grant_type': 'password', 'username': 'Reechyy', 'password': pw}
+    auth = requests.auth.HTTPBasicAuth('i91C4OUnzMQ5MZRA40NKeA', '4AJCAox0saa_4O59WIXHqoaq-GLI3g')
+    resp = requests.post(base_url + 'api/v1/access_token',
+                         data=data,
+                         headers={'User-Agent': 'NewsFeedGUI'},
+		                 auth=auth)
+    d = resp.json()
+    token = 'bearer ' + d['access_token']
+
+    oauth_url = 'https://oauth.reddit.com'
+
+    headers = {'Authorization': token, 'User-Agent': 'NewsFeedGUI'}
+    payload = {'q': subject, 'limit': 5, 'sort': 'new'}
+
+    news_data = []
+    index = 0
+
+    response = requests.get(oauth_url + '/r/news/search', headers=headers, params=payload)
+    if response.status_code == 200:
+        news = response.json()
+        for post in news['data']['children']:
+            index += 1
+            post_data = post['data']
+            title = post_data['title']
+            link = post_data['url']
+            source = post_data['subreddit_name_prefixed']
+            news_data.append((index, source, title, link))
+        
+    return news_data
