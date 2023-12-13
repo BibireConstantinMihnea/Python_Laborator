fcc3ec90f2ccbb554d9dd438f1d45a9efff588f7
BibireConstantinMihnea <100199858+BibireConstantinMihnea@users.noreply.github.com>
2023-12-13 16:17:14 +0200

    Added New York Times API

--- Proiect/api_requests.py
+++ Proiect/api_requests.py
@@ -5,7 +5,7 @@ def reddit_news_fetch(subject):
     with open('pw.txt', 'r') as f:
         pw = f.read()
     data = {'grant_type': 'password', 'username': 'Reechyy', 'password': pw}
-    auth = requests.auth.HTTPBasicAuth('i91C4OUnzMQ5MZRA40NKeA', '4AJCAox0saa_4O59WIXHqoaq-GLI3g')
+    auth = requests.auth.HTTPBasicAuth('dLDgu8S4xVHnkfWoDfej_Q', 'd5pDT9sxLrtY4qb-gZlWoUWduIWNWg')
     resp = requests.post(base_url + 'api/v1/access_token',
                          data=data,
                          headers={'User-Agent': 'NewsFeedGUI'},
@@ -19,20 +19,41 @@ def reddit_news_fetch(subject):
     search_params = {'q': subject, 'limit': 10, 'sort': 'new', 'restrict_sr': 'True'}
 
     news_data = []
-    index = 0
 
     response = requests.get(oauth_url + '/r/news/search', headers=headers, params=search_params)
     if response.status_code == 200:
         news = response.json()
         for post in news['data']['children']:
-            index += 1
             post_data = post['data']
             title = post_data['title']
             link = post_data['url']
             source = "Reddit - " + post_data['subreddit_name_prefixed']
-            news_data.append((index, source, title, link))
+            news_data.append((source, title, link))
         
     return news_data
 
-def other_news_fetch():
-    pass
+def nyt_news_fetch(subject):
+    api_key = '5oGXRF3JyvHUXmaVgzLiNluTq1p8OLys'
+    base_url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
+    
+    params = {
+        'q': subject,
+        'api-key': api_key,
+        'sort': 'relevance',
+        'fl': 'web_url,headline,source',
+        'fq': subject
+    }
+
+    response = requests.get(base_url, params=params)
+
+    news_data = []
+
+    if response.status_code == 200:
+        articles = response.json()['response']['docs']
+        for article in articles:
+            title = article['headline']['main']
+            link = article['web_url']
+            source = article['source']
+            news_data.append((source, title, link))
+
+    return news_data
--- Proiect/gui.py
+++ Proiect/gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from api_requests import reddit_news_fetch
+from api_requests import reddit_news_fetch, nyt_news_fetch
 
 class NewsFeed:
     def __init__(self, mainframe):
@@ -24,12 +24,14 @@ class NewsFeed:
     def get_news(self):
         subject = self.entry.get()
         if subject:
-            news_data = reddit_news_fetch(subject)
+            news_data_reddit = reddit_news_fetch(subject)
+            news_data_nyt = nyt_news_fetch(subject)
+            news_data = news_data_reddit + news_data_nyt
             self.display_news(news_data)
 
     def display_news(self, news_data):
-       self.text_widget.delete(1.0, tk.END)  # Clear previous content
+       self.text_widget.delete(1.0, tk.END)
 
-       for index, source, title, link in news_data:
-            display_text = f"{index}. {title}\nSursa: {source}\nLink: {link}\n\n"
+       for source, title, link in news_data:
+            display_text = f"{title}\nSursa: {source}\nLink: {link}\n\n"
             self.text_widget.insert(tk.END, display_text)
